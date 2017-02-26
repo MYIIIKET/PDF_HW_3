efbd7e7bf5d936c0a53170f1baa2741da97ad284
MYIIIKET <mylllket@yandex.ua>
2017-02-26 17:39:43 +0300

    minor fixes

--- task3_2/src/main/java/com/mylllket_inc/app/Main.java
+++ task3_2/src/main/java/com/mylllket_inc/app/Main.java
@@ -8,8 +8,8 @@ public class Main {
 
     public static void main(String[] args) {
 //        Locale en = new Locale("en", "US");
-        Locale ru = new Locale("ru","ru");
-        ResourceBundle rb1 = ResourceBundle.getBundle("Questions", ru);
+        Locale ru = new Locale("ru","RU");
+        ResourceBundle rb1 = ResourceBundle.getBundle("com.mylllket_inc.app.Questions_ru_RU", ru);
 
 
 //        System.out.println(rb1.getString("question_1"));
