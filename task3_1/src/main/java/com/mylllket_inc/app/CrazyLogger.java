8392ca8a655045967cea86f12fb10dd318765232
MYIIIKET <mylllket@yandex.ua>
2017-02-26 11:49:58 +0300

    search by date added

--- task3_1/src/main/java/com/mylllket_inc/app/CrazyLogger.java
+++ task3_1/src/main/java/com/mylllket_inc/app/CrazyLogger.java
@@ -1,11 +1,14 @@
 package com.mylllket_inc.app;
 
+import java.time.DateTimeException;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public class CrazyLogger {
     private StringBuilder log;
-    private static final DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-mm-YYYY:hh-mm");
+    private static final DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-YYYY:hh-mm");
 
     public void addEvent(String message) {
         if (log == null)
@@ -14,7 +17,32 @@ public class CrazyLogger {
         log.append(message + "\n");
     }
 
+    public String searchByDate(String toSearch) {
+        if (!checkOnFormat(toSearch))
+            return null;
+        StringBuilder toReturn = new StringBuilder();
+        String[] result = log.toString().split("\\n");
+        for (int i = 0; i < result.length; i++) {
+            if (log.substring(0, 16).equals(toSearch))
+                toReturn.append(result[i] + "\n");
+        }
+        if (toReturn.toString().equals(""))
+            return toReturn.append("Nothing").toString();
+        return toReturn.toString();
+    }
+
     public String getInfo() {
         return log.toString();
+    }
+
+    private boolean checkOnFormat(String data) {
+        try {
+            format.parse(data);
+            return true;
+        } catch (DateTimeParseException e) {
+            System.out.println("Wrong date format!");
+            return false;
+        }
+
     }
 }
--- task3_1/src/test/java/com/mylllket_inc/app/CrazyLoggerTest.java
+++ task3_1/src/test/java/com/mylllket_inc/app/CrazyLoggerTest.java
@@ -3,6 +3,14 @@ package com.mylllket_inc.app;
 import junit.framework.TestCase;
 
 public class CrazyLoggerTest extends TestCase {
+    public void testSearchByDate() throws Exception {
+        CrazyLogger a = new CrazyLogger();
+        a.addEvent("aaa");
+        a.addEvent("bbb");
+        System.out.println(a.getInfo());
+        System.out.println(a.searchByDate("26-02-2017:11-48"));
+    }
+
     public void testAddEvent() throws Exception {
         CrazyLogger a = new CrazyLogger();
         a.addEvent("aaa");
